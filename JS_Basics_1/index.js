eb5fc5f8544deb35c76179cbdaea40ae05d04ab8
bharatprajapati111 <prajapatipdd111@gmail.com>
2023-02-08 20:44:32 +0530

    javascript begin

---
+++ JS_Basics_1/index.js
@@ -0,0 +1,10 @@
+//line below is used to print on the console window
+console.log('Namaste Duniya, Hum Raja hai is duniya k'); 
+let a=9;
+console.log(a);
+
+let Lname='babber';
+console.log(Lname);
+
+Lname=5;
+console.log(Lname);
